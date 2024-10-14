b2cc51d21a0c2534eed904bca812a41ad26dbb0a
Owaania <0014020@student.isf.edu.hk>
2024-10-14 11:54:34 +0800

    #today I wrote the MVP models and tested it, #next I will begin working on the mvp for the views.py, like adding new songs and letting users see the list of all songs.

--- app/models.py
+++ app/models.py
@@ -1,3 +1,34 @@
 # models.py
 
-from banjo.models import Model, StringField, IntegerField, FloatField, BooleanField
+from banjo.models import Model, StringField, IntegerField
+
+class Song(Model):
+    title = StringField()
+    artist = StringField()
+    # likes = IntegerField()
+    # dislikes = IntegerField()
+    # spotify_streams = IntegerField()
+    # description = StringField()
+
+
+
+    def json_response(self):
+        
+        return {
+            'id': self.id,
+            'title': self.title,
+            'artist':self.artist
+            # 'likes': self.likes,
+            # 'dislikes': self.dislikes,
+            # 'streams': self.spotify_streams, 
+        }
+
+    def increase_likes(self):
+        self.likes += 1
+        self.save()
+           
+    def increase_dislikes(self):
+        self.dislikes += 1
+        self.save()
+
+ 
--- app/views.py
+++ app/views.py
@@ -2,4 +2,26 @@
 
 from banjo.urls import route_get, route_post
 from settings import BASE_URL
+from .models import Song
 
+@route_post(BASE_URL + 'new', args={'title':str, 'artist': str})
+def new_song(args):
+    new_song = Song(
+        title = args['title'],
+        artist = args['artist'],
+        # likes = 0,
+        # archive = False,
+    )
+
+    new_song.save()
+
+    return {'fortune': new_song.json_response()}
+
+@route_get(BASE_URL + 'all')
+def all_fortunes(args):
+    song_list = []
+
+    for fortune in Fortune.objects.all():
+        fortunes_list.append(fortune.json_response())
+
+    return {'fortunes':fortunes_list}
